c931a0b575cf9bdc244965fafd6437800fa41d4b
Rudra <rudrahimangshoo2020@gmail.com>
2024-02-09 01:01:55 +0600

    setup

---
+++ app/components/Ractangle.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Ractangle = () => {
+  return (
+    <View>
+      <Text>Ractangle</Text>
+    </View>
+  )
+}
+
+export default Ractangle
+
+const styles = StyleSheet.create({})
---
+++ app/components/grid.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const grid = () => {
+  return (
+    <View>
+      <Text>grid</Text>
+    </View>
+  )
+}
+
+export default grid
+
+const styles = StyleSheet.create({})
