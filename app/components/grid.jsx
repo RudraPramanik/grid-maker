e413e453ce8d92bb3ffc69f0bbe9e9e25974225a
Rudra <rudrahimangshoo2020@gmail.com>
2024-02-09 02:14:43 +0600

    input

--- app/components/grid.jsx
+++ app/components/grid.jsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const grid = () => {
+const Grid = () => {
   return (
     <View>
       <Text>grid</Text>
@@ -9,6 +9,6 @@ const grid = () => {
   )
 }
 
-export default grid
+export default Grid
 
 const styles = StyleSheet.create({})
--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -1,14 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 
-const home = () => {
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import Grid from '../components/grid';
+
+
+
+const Home = () => {
+
   return (
-    <View>
-      <Text>home</Text>
+    <View style={styles.container}>
+        <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter number of columns"
+        value=""
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter number of rows"
+        value=""
+        keyboardType="numeric"
+      />
+      <TouchableOpacity>submit</TouchableOpacity>
+      </View>
+      <View style={styles.gridContainer}>
+        <Grid/>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default home
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    flex: 2,
+    width: '100%',
+  },
+  gridContainer: {
+    flex: 3,
+    width:'100%',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius:10,
+    width: '100%',
+  },
 
-const styles = StyleSheet.create({})
+ 
+});
+
+export default Home;
+
+
